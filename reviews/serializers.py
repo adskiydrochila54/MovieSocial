5e318a3a62e74c7ea258c64949322ea1899fcc9c
adskiydrochila54 <bloopersgag@gmail.com>
2025-09-23 18:02:11 +0600

    added apps movies and reviews

---
+++ reviews/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Review, Comment, ReviewLike
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "content", "created_at"]
+        read_only_fields = ["author"]
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = [
+            "id",
+            "movie",
+            "author",
+            "title",
+            "content",
+            "rating",
+            "created_at",
+            "comments",
+            "likes_count",
+        ]
+        read_only_fields = ["author"]
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+class ReviewLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewLike
+        fields = ["id", "review", "user", "created_at"]
+        read_only_fields = ["user"]
--- users/signals.py
+++ users/signals.py
@@ -6,15 +6,11 @@ from .models import Profile, Genre
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def manage_user_profile(sender, instance, created, **kwargs):
-    """Создание или сохранение профиля пользователя"""
     if created:
-        # Создаем профиль
         profile = Profile.objects.create(user=instance)
 
-        # Добавляем жанры по умолчанию (если они есть в БД)
         default_genres = Genre.objects.filter(name__in=["Драма", "Комедия"])
         profile.favorite_genres.set(default_genres)
     else:
-        # Обновляем профиль, если он уже существует
         if hasattr(instance, 'profile'):
             instance.profile.save()
